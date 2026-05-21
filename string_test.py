1c4eace84e1abd4715862cc1edfd334c367dc12f
krishna <krishnascm21@gmail.com>
2026-05-21 12:19:42 +0530

    added new string

--- string_test.py
+++ string_test.py
@@ -14,3 +14,20 @@ replace = name.replace("Krishna","Chinu")
 print(replace)
 
 print(name.strip())
+
+text = "\n\nPython\n"
+print(text)
+print(text.strip())
+
+text1 = "###welcome###"
+print(text1)
+print(text1.strip("e"))
+
+####Slice"
+
+text = "apple banana mango"
+
+print(text)
+words = text.split()
+
+print(words[0])
