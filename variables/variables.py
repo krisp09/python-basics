457267323a403841b8b2867a013285045060e450
krishna <krishnascm21@gmail.com>
2026-05-21 11:48:23 +0530

    adding variables

---
+++ variables/variables.py
@@ -0,0 +1,10 @@
+name = "Krishna"
+experience = 10
+salary = 250000
+is_engineer = True
+
+print(name)
+print(type(name))
+print(type(experience))
+print(type(salary))
+print(type(is_engineer))
---
+++ variables/variables_pr.py
@@ -0,0 +1,10 @@
+name = "Krishna"
+number = 20
+salary = "25000"
+is_engineer = True
+
+print(name)
+print(type(name))
+print(type(number))
+print(type(salary))
+print(type(is_engineer))
