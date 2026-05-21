059d4a02e2f7dc14ceaa62dcbc0079f1a773f385
krishna <krishnascm21@gmail.com>
2026-05-21 12:14:06 +0530

    added string

---
+++ string_test.py
@@ -0,0 +1,16 @@
+name = "Krishna yadav  "
+
+print(f"Hello {name}")
+
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+print(name.replace("Krishna","Mahesh"))
+
+print("----")
+
+replace = name.replace("Krishna","Chinu")
+print(replace)
+
+print(name.strip())
