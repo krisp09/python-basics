f317f8d2e474555e4754f675c25c47c239573104
krishna <krishnascm21@gmail.com>
2026-05-21 12:05:48 +0530

    added string

---
+++ strings.py
@@ -0,0 +1,24 @@
+name = "krishna paka"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(len(name))
+
+print(name.replace("krishna", "K"))
+
+print("--------------")
+
+
+name1 ="Mahesh babu"
+
+print(name1)
+
+print(name1.capitalize())
+print(name1.upper())
+print(name1.lower())
+print(name1.title())
+print(len(name1))
+new_name = print(name1.replace("Mahesh","R"))
+print(new_name)
