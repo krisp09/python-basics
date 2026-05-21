e850a1ce607b787dc601769755f9b14d6cc27469
krishna <krishnascm21@gmail.com>
2026-05-21 16:49:51 +0530

    added the dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+employee = {
+    "name": "Krishna",
+    "role": "Cloud Engineer",
+    "experience": 10
+}
+
+print(employee)
+
+print(employee["name"])
+
+employee["salary"] = 250000
+
+print(len(employee))
